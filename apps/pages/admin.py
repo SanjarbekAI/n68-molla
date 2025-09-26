df011fe9ea32b83c42de2d2559d21646b1b7cad3
Faxriddinbek <faxriddinorinboyev12@gmail.com>
2025-09-26 17:11:53 +0500

    store_model

--- apps/pages/admin.py
+++ apps/pages/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from apps.pages.models import ContactModel
+from apps.pages.models import ContactModel, StoreModel
 
 admin.site.register(ContactModel)
+
+@admin.register(StoreModel)
+class StoreModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'location', 'phone_number', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('is_active', 'location')
+    search_fields = ('title', 'location', 'phone_number')
+    ordering = ('-id',)
